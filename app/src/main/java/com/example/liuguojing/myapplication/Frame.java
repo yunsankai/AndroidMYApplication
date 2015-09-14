7d86797adec25a9aefed004aee605421d29f5efb
7_29liuguojing <liuguojing@gameley.com>
2015-09-14 15:30:24 +0800

    first commit

--- app/src/main/java/com/example/liuguojing/myapplication/Frame.java
+++ app/src/main/java/com/example/liuguojing/myapplication/Frame.java
@@ -14,7 +14,7 @@ public class Frame {
     public static void LogFame(Frame frame){
         Log.d("DEBUG","frame is x="+frame.x+"\ny="+frame.y+"\nwidth="+frame.width+"\nheight="+frame.height);
     }
-
+    //I change the frame
     Frame(int _x, int _y, int _width ,int _height){
         x = _x;y = _y;width = _width;height = _height;
     }
